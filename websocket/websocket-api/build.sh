5fc9bcb3bbf0655a3ac577329dd24677b474101e
Elias Meireles <eliasmflilico@gmail.com>
2023-03-26 18:44:37 +0000

    Configurando scripts de deploy

---
+++ websocket/websocket-api/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+rm -rf ~/.websocker_deploy/
+mkdir ~/.websocker_deploy/
+cp -r . ~/.websocker_deploy/
+cd ~/.websocker_deploy/
+
+rm -rf .git
+rm -rf spring-boot-included-builds
+git init
+git submodule add --force https://github.com/eliasmeireles/spring-boot-included-builds.git  spring-boot-included-builds
+git submodule update --recursive --init
+git submodule update --recursive --remote
+
+./gradlew bootJar
+
+cp ./build/libs/websocket-api-1.0.0.jar ./deploy/runner.jar
+
+cd ./deploy
+
+docker-compose build websocket-api
+
+rm -rf ~/.websocker_deploy/
