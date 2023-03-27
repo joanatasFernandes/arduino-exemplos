ed9dc8ccea946cda1f612c9a94c13e2d3f6509e0
Elias Meireles <eliasmflilico@gmail.com>
2023-03-26 22:47:12 -0300

    Melhorias do deploy

--- websocket/websocket-api/src/main/kotlin/br/com/arduino/api/controller/HealthCheckController.kt
+++ websocket/websocket-api/src/main/kotlin/br/com/arduino/api/controller/HealthCheckController.kt
@@ -1,12 +1,17 @@
 package br.com.arduino.api.controller
 
 import br.com.arduino.api.model.HealthCheck200Response
+import com.softwareplace.json.logger.log.JsonLog
+import com.softwareplace.json.logger.log.kLogger
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthCheckController : HealthApi {
     override fun healthCheck(): ResponseEntity<HealthCheck200Response> {
+        JsonLog(kLogger)
+            .message("Health checking.")
+            .run()
         return ResponseEntity.ok(HealthCheck200Response("Application is running"))
     }
 }
