f2276a84407ab9fd7124f5fb948c5156156aa133
Elias Meireles <eliasmflilico@gmail.com>
2023-03-28 20:16:37 -0300

    Migrando docker deploy para usar o compose

--- websocket/websocket-api/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-if wget -O ./run.jar https://github.com/eliasmeireles/arduino-exemplos/raw/main/websocket/websocket-api/runner.jar; then
-      java -jar run.jar
-fi
-
-
--- websocket/websocket-api/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-docker container rm -f websocket-api
-docker run --name websocket-api -d -p 8080:8080 websocket-api:latest
